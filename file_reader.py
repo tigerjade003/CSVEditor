b177e8c9dc6e1fc62daf0159aef62831cff79ca4
tigerjade003 <tigerjade003@gmail.com>
2026-03-27 21:09:36 -0700

    added reading and writing with basic catches

--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
 def get_data(path = None):
-    pass
+    if path is None:
+        raise ValueError("Path must be provided.")
+    if not path.endswith('.csv'):
+        raise ValueError("File must be a CSV file.")
+    data = pd.read_csv(path)
+    return data
+
+def write_data(data = None, path = None):
+    if data is None:
+        raise ValueError("Data must be provided.")
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError("Data must be a pandas DataFrame.")
+    if path is None:
+        raise ValueError("Path must be provided.")
+    data.to_csv(path, index=False)
